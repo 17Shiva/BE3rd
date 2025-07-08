d2ab4d8870f847b30b6243d29435fc517d29d1f3
17Shiva <157380849+17Shiva@users.noreply.github.com>
2025-07-08 15:00:41 +0530

    Lect3 Package Publish

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const fi1 = require('./file1')
-const fi2 = require("./file2")
-
-// it must be stuck in loop
-
-console.log(fi1);
-console.log(fi2);
-    
-// cahce kr ke rakh leta h
+function sum(a,b){
+    return a+b;
+}
+module.exports = sum;
---
+++ working.js
@@ -0,0 +1,2 @@
+const sum = require('lect3');
+console.log(sum(9, 9)); 
