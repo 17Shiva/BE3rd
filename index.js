4476e0a01e314a1c88a496fd3c90cd766eff7089
17Shiva <157380849+17Shiva@users.noreply.github.com>
2025-07-16 14:26:00 +0530

    Lect6 work

--- index.js
+++ index.js
@@ -1,73 +1,14 @@
-let users= [
-    {
-        id:1,
-        name:"vansh",
-        age:"24"
-    },
-    {
-        id:2,
-        name:"Ritik",
-        age:19
-    },
-    {
-        id:3,
-        name:"Ritik",
-        age:16
-    }
-]
-
-
-function isAllowed(id){
-    // id ka user
-    let user = users.find(u => u.id ==id);
-        if (!user) {
-        console.log("User not found");
-        return;
-    }
-
-        if(user.age >= 18){
-            console.log(user.name)
-            console.log("valid for voting")
-       
+const fs = require('fs');
+fs.writeFile('../demo.txt',"G26 Hello",function(err,data){ // this function run when file gets readed
+        if(err){
+            return console.log(err);
         }
-        else{
-             console.log("Not valid")
-        }
-    
-   
-}
-
-function isAllowedfor(id){
-for(let i=0;i<users.length;i++){
-    if(users[i].id==id){
-        if(users[i].age>=18){
-            console.log("Allowed for voting")
-        }else{
-            console.log("Not allowed for voting")
-        }
-    }
-}
-   
-}
-function promiseAllowed(id){
-    return new Promise((resolve,reject)=>{
-        let user = users.find((u) =>{
-            return u.id==id;
-        })
-        console.log(user);
-        if(!user) return reject("No user found")
-            if(user.age<18) return  reject ("Age is less");
-        return resolve("Eligible to vote ");
-    })
-}promiseAllowed(3).then((data)=>{
-    console.log(data)
+        console.log("Succesfully done "); // data nhi ayega kyuiki huma write kar rhe h ...
 })
-.catch((err=>{
-    console.log(err);
-}))
 
-
-
-
-isAllowed(2);
-isAllowedfor(2);
+fs.writeFile('../demo2.txt',"Creating new File and writing something ",function(err){ // this function run when file gets readed
+        if(err){
+            return console.log(err);
+        }
+        console.log("Succesfully done "); // data nhi ayega kyuiki huma write kar rhe h ...
+})
