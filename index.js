c7db8d526fa30338028d97f7f169e88d0019e125
17Shiva <157380849+17Shiva@users.noreply.github.com>
2025-07-21 15:36:12 +0530

    Lect8 Work Express , Path variable -> Query,params

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const fs = require('fs');
-fs.writeFile('../demo.txt',"G26 Hello",function(err,data){ // this function run when file gets readed
-        if(err){
-            return console.log(err);
-        }
-        console.log("Succesfully done "); // data nhi ayega kyuiki huma write kar rhe h ...
+const express = require('express')
+const app = express();
+
+app.get('/',(req,res) => {
+    // res.send("Hello world")
+    res.send("<h1>Hello</h1>")
+    // res.send(__dirname+'/index.html')
+
+    // res.json({
+    //     name:"Nitesh",
+    //     age: 21
+    // })
+    // res.end("hi")
+
+
 })
 
-fs.writeFile('../demo2.txt',"Creating new File and writing something ",function(err){ // this function run when file gets readed
-        if(err){
-            return console.log(err);
-        }
-        console.log("Succesfully done "); // data nhi ayega kyuiki huma write kar rhe h ...
-})
+// Path variable 
+ // Query Parameters
+app.get('/watch' , (req,res)=>{
+    let videoId = req.query.v;
+    let nid = req.query.n;
+    res.send("id got it",videoId,nid);
+} )
+
+
+ // Params
+
+ app.get('watch/:v',(req,res)=>{
+    console.log(req.params.v) 
+    console.log(req.params.n)
+    res.send("got it !!!!!");
+ })
+
+app.listen(3001,()=>{
+    console.log("server running at http://localhost:3001 ");
+}); // starts the server on port 3001
